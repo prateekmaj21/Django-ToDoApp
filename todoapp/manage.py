addf8d3248579197e5be267d17e999e79308856f
Prateek Majumder <52664250+prateekmaj21@users.noreply.github.com>
2021-04-10 20:58:23 +0530

    uploaded files

---
+++ todoapp/manage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. "
+            
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+
+#Done by Prateek Majumder
+#Django App Project
+#Simple ToDoApp
